85ce92744cbeea48f9dd05de6897d65a0335fc29
Ali Akkas <aliakkas006@gmail.com>
2021-10-29 17:18:48 +0600

    delivery men added

--- src/pages/Home/DeliveryMan/DeliveryMan.js
+++ src/pages/Home/DeliveryMan/DeliveryMan.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
 const DeliveryMan = () => {
+    const [deliveryMan, setDeliveryMan] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/delivery')
+            .then(res => setDeliveryMan(res.data));
+    }, []);
+
     return (
-        <div>
-            <h2>This is delivery man div</h2>
-        </div>
+        <Container>
+            <h2 className="text-center my-3">Our Delivery Men</h2>
+            <Row xs={1} md={3} className="g-4 mb-5">
+                {
+                    deliveryMan.map(dm => <Col key={dm._id}>
+                        <Card className="h-100">
+                            <Card.Img variant="top" src={dm.img} />
+                            <Card.Body>
+                                <Card.Title> {dm.name} </Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>)
+                }
+            </Row>
+        </Container>
     );
 };
 
